519c747e06830a9fb17b937a15856818440b6b64
Pierre-Louis Garnerone <62054882+MutMutt@users.noreply.github.com>
2021-03-25 20:43:22 +0100

    Context and book model

---
+++ backend-book-assignement/Models/Books.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace backend_book_assignement.Models
+{
+    public class Books
+    {
+        [Key]
+        public int id { get; set; }
+        public decimal price { get; set; }
+        public string isbn { get; set; }
+    }
+}
